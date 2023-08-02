4c9176d8ba8ac96ccd5d639cd396cd971a1ff2d3
d2weber <29163905+d2weber@users.noreply.github.com>
2023-08-02 17:52:05 +0200

    Declare themes inline

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use crate::scheme_preference::SchemePreference;
 use crate::settings_proxy::SettingChanged;
 use crate::settings_proxy::SettingsProxy;
 use smol::fs;
-use smol::fs::read_to_string;
 use smol::fs::File;
 use smol::future;
+use smol::io::AsyncBufReadExt;
 use smol::io::AsyncWriteExt;
+use smol::io::BufReader;
+use smol::prelude::AsyncRead;
+use smol::prelude::AsyncWrite;
 use smol::process::Command;
 use smol::stream::StreamExt;
-use toml_edit::{value, Document};
 use zbus::Connection;
 
 mod scheme_preference;
@@ -57,26 +59,63 @@ async fn set_theme_helix(p: SchemePreference) -> std::io::Result<()> {
         path
     };
 
-    let mut config = read_to_string(&path)
-        .await?
-        .parse::<Document>()
-        .expect("Couldn't parse config file");
-
-    let theme = match p {
-        SchemePreference::Dark => "adwaita-dark",
-        _ => "onelight",
-    };
-    config["theme"] = value(theme);
-
     let tmp_path = path.with_extension("tmp.toml");
+    let mut out = File::create(&tmp_path).await?;
 
-    let mut file = File::create(&tmp_path).await?;
-    file.write(config.to_string().as_bytes()).await?;
-    file.flush().await?;
+    if let e @ Err(_) = adjust_config(File::open(&path).await?, &mut out, p).await {
+        out.close().await?;
+        return e;
+    };
+
+    out.flush().await?;
 
     fs::rename(tmp_path, path).await?;
     Command::new("pkill").args(["-USR1", "hx"]).status().await?;
 
+    Ok(())
+}
+
+async fn adjust_config(
+    inp: impl AsyncRead + Unpin,
+    mut out: impl AsyncWrite + Unpin,
+    p: SchemePreference,
+) -> Result<(), std::io::Error> {
+    let [mut old_tag, mut new_tag] = match p {
+        SchemePreference::Dark => ["#[light]", "#[dark]"],
+        _ => ["#[dark]", "#[light]"],
+    }
+    .map(Some);
+
+    let mut lines = BufReader::new(inp).lines();
+    while let Some(Ok(line)) = lines.next().await {
+        let trimmed = line.trim();
+        if new_tag.is_some_and(|t| trimmed.ends_with(t)) {
+            new_tag.take().unwrap(); // Mark as found
+            if trimmed.starts_with('#') {
+                let (trailing_whitespaces, rest) = line.split_once('#').unwrap();
+                out.write_all(trailing_whitespaces.as_bytes()).await?;
+                out.write_all(rest.strip_prefix(' ').unwrap_or(rest).as_bytes())
+                    .await?;
+            } else {
+                out.write_all(line.as_bytes()).await?;
+            }
+        } else {
+            if old_tag.is_some_and(|t| trimmed.ends_with(t)) {
+                old_tag.take().unwrap(); // Mark as found
+                if !trimmed.starts_with('#') {
+                    out.write_all(b"# ").await?;
+                }
+            }
+            out.write_all(line.as_bytes()).await?;
+        }
+        out.write_all(b"\n").await?;
+    }
+    if let Some(t) = old_tag.or(new_tag) {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            format!("Tag `{t}` not found"),
+        ));
+    }
     Ok(())
 }
 
