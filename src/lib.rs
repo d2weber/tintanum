00b9873ef8d2b94dd61fb519c6695e33acc0f7ad
Hunter Beast <hunter@cryptoquick.com>
2024-12-08 10:05:57 -0700

    Update src/lib.rs
    Co-authored-by: d2weber <29163905+d2weber@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -40,8 +40,8 @@ impl<'s> TryFrom<&'s zbus::Message> for SchemePreference {
     type Error = Error;
     fn try_from(message: &'s zbus::Message) -> Result<Self> {
         let body = message.body();
-        let value: zvariant::Value = body.deserialize()?;
-        SchemePreference::try_from(value)
+        let args: (&str, &str, zvariant::Value<'_>) = body.deserialize()?;
+        SchemePreference::try_from(args.2)
     }
 }
 
