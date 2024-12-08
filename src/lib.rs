ec4ef6d7e6ee0bd2e914188fd3721bee585229fa
Hunter Beast <hunter@cryptoquick.com>
2024-12-08 12:57:41 -0700

    Update crates. zbus 3 -> zbus 5. (#1)
    * Update crates. zbus 3 -> zbus 5.
    
    * Update src/lib.rs
    
    Co-authored-by: d2weber <29163905+d2weber@users.noreply.github.com>
    
    * Syntax nitpick
    
    * One last update
    
    ---------
    
    Co-authored-by: d2weber <29163905+d2weber@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use futures_lite::prelude::*;
 use futures_lite::stream::once;
+use zbus::proxy;
 use zbus::zvariant;
 use zbus::zvariant::OwnedValue;
 use zbus::Connection;
 use zbus::Error;
-use zbus::ProxyBuilder;
 use zbus::Result;
 
 #[cfg(feature = "serde")]
@@ -39,19 +39,26 @@ impl TryFrom<zvariant::Value<'_>> for SchemePreference {
 impl<'s> TryFrom<&'s zbus::Message> for SchemePreference {
     type Error = Error;
     fn try_from(message: &'s zbus::Message) -> Result<Self> {
-        message
-            .body::<(&str, &str, zvariant::Value<'_>)>()
-            .and_then(|args| SchemePreference::try_from(args.2))
+        let body = message.body();
+        let (_, _, arg): (&str, &str, zvariant::Value<'_>) = body.deserialize()?;
+        SchemePreference::try_from(arg)
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct SchemeProxy<'a>(zbus::Proxy<'a>);
 
-impl<'a> ::zbus::ProxyDefault for SchemeProxy<'a> {
-    const INTERFACE: &'static str = "org.freedesktop.portal.Settings";
-    const DESTINATION: &'static str = "org.freedesktop.portal.Desktop";
-    const PATH: &'static str = "/org/freedesktop/portal/desktop";
+impl<'a> ::zbus::proxy::Defaults for SchemeProxy<'a> {
+    const INTERFACE: &'static Option<zbus::names::InterfaceName<'static>> = &Some(
+        zbus::names::InterfaceName::from_static_str_unchecked("org.freedesktop.portal.Settings"),
+    );
+    const DESTINATION: &'static Option<zbus::names::BusName<'static>> =
+        &Some(zbus::names::BusName::WellKnown({
+            zbus::names::WellKnownName::from_static_str_unchecked("org.freedesktop.portal.Desktop")
+        }));
+    const PATH: &'static Option<zbus::zvariant::ObjectPath<'static>> = &Some(
+        zbus::zvariant::ObjectPath::from_static_str_unchecked("/org/freedesktop/portal/desktop"),
+    );
 }
 
 impl<'c> From<zbus::Proxy<'c>> for SchemeProxy<'c> {
@@ -69,17 +76,16 @@ impl<'a> SchemeProxy<'a> {
         Self::new(&zbus::Connection::session().await?).await
     }
 
-    pub fn builder(conn: &::zbus::Connection) -> ProxyBuilder<'a, Self> {
-        ProxyBuilder::new(conn).cache_properties(zbus::CacheProperties::No)
+    pub fn builder(conn: &::zbus::Connection) -> proxy::Builder<'a, Self> {
+        proxy::Builder::new(conn).cache_properties(zbus::proxy::CacheProperties::No)
     }
 
     pub async fn read(&self) -> Result<SchemePreference> {
         let reply: OwnedValue = self.0.call("Read", &(NAMESPACE, KEY)).await?;
-        reply
-            .downcast_ref::<zvariant::Value>()
-            .cloned()
-            .ok_or(zvariant::Error::IncorrectType.into())
-            .and_then(SchemePreference::try_from)
+        reply.downcast_ref::<zvariant::Value>().map_or_else(
+            |_| Err(zvariant::Error::IncorrectType.into()),
+            SchemePreference::try_from,
+        )
     }
 
     // Can contain duplicates
@@ -88,7 +94,7 @@ impl<'a> SchemeProxy<'a> {
             .0
             .receive_signal_with_args("SettingChanged", &[(0, NAMESPACE), (1, KEY)])
             .await?
-            .filter_map(|x| SchemePreference::try_from(x.as_ref()).ok());
+            .filter_map(|x| SchemePreference::try_from(&x).ok());
 
         Ok(signal)
     }
